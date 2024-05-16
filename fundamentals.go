d6a1151c5448f98462900c0bf1eb197b7cd20ec5
Stas Klymenko <klymenko.stas@gmail.com>
2024-05-16 22:25:03 +0300

    Playing with prints

---
+++ fundamentals.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Print("Hello, world! \n")
+	fmt.Println(math.Pi)
+	fmt.Printf("Circle diameter is 10m. The perimeter of circle is: %v", math.Pi*10)
+}
